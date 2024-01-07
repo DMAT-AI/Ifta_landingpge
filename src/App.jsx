8b3c1e7b5796d84f26bfbd0081dc35dadf36b7ea
Derby <dmatderby@gmail.com>
2024-01-07 21:15:58 +0400

    working out so far

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     <KeyFeatures />
     <HowItWorks />
     {/* <FormData /> FORM handling is done using forbold for now */}
-    <h1 className="container flex justify-center font-extrabold text-transparent text-7xl bg-clip-text bg-gradient-to-r from-purple-400 to-green-600 mb-5 ">Contact Us</h1>
+    {/* <h1 className="container flex justify-center font-extrabold text-transparent text-7xl bg-clip-text bg-gradient-to-r from-purple-400 to-green-600 mb-5 ">Contact Us</h1> */}
     <Video />
 
   </>
